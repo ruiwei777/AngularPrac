e7b4942d4e27cce95376cdb6d23d7d6f8d6c3e7f
Ruiwei <liangruiwei777@gmail.com>
2017-07-16 21:31:26 +1000

    Initial commit

---
+++ src/app/heroes.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Hero } from './hero'
+import { HeroService } from './hero.service';
+
+@Component({
+  selector: 'my-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+  // providers: [HeroService],  // now in app.modules.ts, can be accessed anywhere
+})
+export class HeroesComponent implements OnInit{
+  
+  selectedHero: Hero;
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService,
+      private router:Router
+      ){ }
+
+  add(name: string): void {
+    name = name.trim();
+      if (!name) { return; }
+      this.heroService.create(name)
+        .then(hero => {
+          this.heroes.push(hero);
+          this.selectedHero = null;
+        });
+  }
+
+  delete(hero: Hero): void {
+    this.heroService
+          .delete(hero.id)
+          .then(() => {
+            this.heroes = this.heroes.filter(h => h !== hero);
+            if (this.selectedHero === hero) { this.selectedHero = null; }
+          });
+  }
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void{
+    this.heroService.getHeroes()
+        .then(heroes => this.heroes = heroes);
+  }
+
+  gotoDetail(): void {
+      this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+}
